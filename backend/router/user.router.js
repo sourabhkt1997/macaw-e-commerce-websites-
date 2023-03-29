f2ad72440ad2c744af59aaaa5c68cc48ef6edc0d
sourabhkt1997 <sourabh.dhanarajan@gmail.com>
2023-03-29 23:29:40 +0530

    day-1

---
+++ backend/router/user.router.js
@@ -0,0 +1,66 @@
+let express=require("express")
+let userrouter=express.Router()
+const bcrypt = require('bcrypt')
+let {UserModel}=require("../model/usermodel")
+let jwt = require('jsonwebtoken')
+
+
+userrouter.post("/register",async(req,res)=>{
+        let {email,password}=req.body
+        console.log(req.body,"g")
+        try{
+            let data=await UserModel.findOne({email})
+            if(data){
+                res.status(400).send({"message":"User already exist, please login"})
+            } 
+        else{
+        bcrypt.hash(password,5,async(err, hash)=>{
+            let userdata= new UserModel({
+                email:email,
+                password:hash
+            })
+            await userdata.save()
+            console.log(userdata,"userdata")
+            res.status(200).send({"message":"signup successfull"})
+        });
+        }
+    }
+    catch(err){
+        res.status(400).send({"message":err.message})
+    }
+})
+
+userrouter.post("/login",async(req,res)=>{
+    let {email,password}=req.body
+   try{
+      let data=await UserModel.find({email})
+      if(data.length>0){
+          bcrypt.compare(password,data[0].password,(err, result)=>{
+              if(result){
+                  res.status(200).send({"message":"login successfull",
+              "token":token = jwt.sign({userid:data[0]._id},'sourabh')})
+              }
+              else{
+                  res.status(400).send({"message":"wrong credentials"})
+              }
+          })
+      }
+      else{
+          res.status(400).send({"message":"register first"})
+      }
+
+   }
+   catch(err){
+      res.status(400).send({"message":err.message})
+   }
+
+})
+
+// userrouter.get()
+
+
+
+
+
+
+module.exports={userrouter}
---
+++ frondend/scripts/index.js
@@ -0,0 +1,112 @@
+let url="http://localhost:8500"
+
+
+let token=localStorage.getItem("logintoken")
+console.log(token)
+
+
+let signinpop=document.getElementById("signinpopup")
+let signuppop=document.getElementById("signuppopup")
+
+let signin =document.getElementById("nav_four")
+
+
+function opensignin(){
+    signinpop.classList.add("opensignin")
+}
+function closesignin(){
+    signinpop.classList.remove("opensignin")
+}
+
+function opensignup(){
+   signuppop.classList.add("opensignup")
+}
+function closesignup(){
+    signuppop.classList.remove("opensignup")
+}
+
+// sign in popup
+let count=0
+signin.addEventListener("click",()=>{
+    count++
+   if(count%2!==0){
+    opensignin()
+   }
+   else{
+    closesignup()
+    closesignin()
+   }
+})
+
+ //signn up popup
+let signup=document.getElementById("signup")
+
+signup.addEventListener("click",()=>{
+    closesignin()
+    opensignup()
+})
+
+let signinalready=document.getElementById("signinalready")
+signinalready.addEventListener("click",()=>{
+    console.log(opensignup())
+    closesignup()
+    opensignin()
+})
+
+
+
+let signupsubmit=document.getElementById("signupsubmit")
+
+
+signupsubmit.addEventListener("click",()=>{
+    let confirmpassword=document.getElementById("signuppasswordconfirm").value
+let password=document.getElementById("signuppassword").value
+console.log(password,confirmpassword)
+if(password===confirmpassword){
+    fetch("http://localhost:8500/users/register",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            email:document.getElementById("signupemail").value,
+            password:document.getElementById("signuppassword").value
+        })
+       })
+       .then(res=>res.json())
+       .then(data=>{
+           let message=document.getElementById("message")
+           message.innerText=data.message
+        
+       }) 
+    }
+    else{
+        let message=document.getElementById("message")
+        message.innerText="please check password"
+    } 
+})
+
+let signinsubmit=document.getElementById("signinsubmit")
+
+signinsubmit.addEventListener("click",()=>{
+    let obj={
+        email:document.getElementById("signinemail").value,
+        password:document.getElementById("signinpassword").value
+     }
+     console.log(obj)
+     let url=`http://localhost:8500/users/login`
+     fetch(url,{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(obj)
+     })
+     .then(res=>res.json())
+     .then(data=>{
+        console.log(data)
+         console.log(data.token)
+         localStorage.setItem("logintoken",data.token)
+     })
+})
+
